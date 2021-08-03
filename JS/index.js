1948d7c9daf459c717a26beae7dede1f5ac74c3a
Juan.Limansky <juanlimansky@hotmail.com>
2021-08-02 21:19:14 -0300

    Se agregó JS

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+/* let nombre = prompt("Ingrese su nombre")
+let edad = Number(prompt("Ingrese su edad"))
+
+if (edad >= 18) {
+    alert("Bienvenido/a " + nombre)
+}
+else if (edad > 0 && edad < 18) {
+    alert("Lo siento " + nombre + ", no tiene la edad suficiente como para ingresar al sitio")
+}
+else {
+    alert("El dato ingresado no es una edad")
+} */
+
+function pedido(){
+    let plato = prompt("¿Qué plato desea ordenar?");
+    alert("Usted ordenó " + plato);
+} 
+pedido();
+pedido();
+pedido();
+
+
+
+
+
