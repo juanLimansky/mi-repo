0933a5893f29a2090f494ef3f8ede9133950e561
Juan.Limansky <juanlimansky@hotmail.com>
2021-08-04 10:14:46 -0300

    se realizó el desafío de la clase 5

--- JS/index.js
+++ JS/index.js
@@ -9,7 +9,9 @@ else if (edad > 0 && edad < 18) {
 }
 else {
     alert("El dato ingresado no es una edad")
-} */
+} 
+
+// DESAFÍO CLASE 4
 
 function pedido(){
     let plato = prompt("¿Qué plato desea ordenar?");
@@ -18,8 +20,29 @@ function pedido(){
 pedido();
 pedido();
 pedido();
+*/
 
 
+// DESAFÍO CLASE 5
 
+class Menu {
+    constructor(entrada, principal, postre) {
+        this.entrada = entrada;
+        this.principal = principal;
+        this.postre = postre;
+    }
+}
+let pedido = prompt("¿Quiere hacer un pedido? si / no");
 
+while (pedido != "no") {
+    inputEntrada = prompt("Ingrese su entrada");
+    inputPrincipal = prompt("Ingrese su plato principal");
+    inputPostre = prompt("Ingrese su postre");
+
+    const menu1 = new Menu (inputEntrada, inputPrincipal, inputPostre);
+
+    console.log(menu1);
+
+    pedido = prompt("¿Quiere hacer un pedido? si / no");
+}
 
